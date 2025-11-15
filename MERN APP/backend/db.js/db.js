40b8369b85b0b1ac5069088ddd71fda4d361e265
Ayush Kumawat <ayushkumawat77889@gmail.com>
2025-11-15 21:04:25 +0530

    compose

--- MERN APP/backend/db.js/db.js
+++ MERN APP/backend/db.js/db.js
@@ -1,8 +1,9 @@
 const  mongoose=require('mongoose');
 
+
 async function connect(){
     try{
-        await mongoose.connect("mongodb+srv://ayushkumawat77889:B8xFdwJTzOlwh3iY@ayush.kq1ew.mongodb.net/newx");
+        await mongoose.connect(process.env.MONGO_URL);
         console.log('Connected to MongoDB');
     }catch(err){
         console.error('Error connecting to MongoDB',err);
